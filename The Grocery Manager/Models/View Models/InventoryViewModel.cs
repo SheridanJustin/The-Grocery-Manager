2fb9007db4bac12d009e525005a92cfee2240d34
SheridanJustin <122491029+SheridanJustin@users.noreply.github.com>
2025-04-15 22:47:21 -0400

    Create InventoryViewModel.cs

---
+++ The Grocery Manager/Models/View Models/InventoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+/// Author: Sannan Ali
+/// InventoryViewModel.cs - ViewModel for displaying and editing inventory in
+/// the Inventory View.
+/// </summary>
+public class InventoryViewModel
+{
+    public string ItemName { get; set; }
+    public double Quantity { get; set; }
+    public string Unit { get; set; }
+    public DateTime LastUpdated { get; set; }
+}

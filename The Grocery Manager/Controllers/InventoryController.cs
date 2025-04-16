c4ff0422332265fd3e0ac90af8ed7d69324a401a
SheridanJustin <122491029+SheridanJustin@users.noreply.github.com>
2025-04-15 23:35:27 -0400

    final

---
+++ The Grocery Manager/Controllers/InventoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using The_Grocery_Manager.Models;
+
+namespace The_Grocery_Manager.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private readonly GroceryDbContext _context;
+
+        public InventoryController(GroceryDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Inventory
+        public IActionResult Index()
+        {
+            int? userId = HttpContext.Session.GetInt32("UserId");
+
+            if (userId == null)
+                return RedirectToAction("Login", "User");
+
+            var inventory = _context.Inventories
+                                    .Where(i => i.UserId == userId)
+                                    .OrderBy(i => i.ItemName)
+                                    .ToList();
+
+            return View(inventory);
+        }
+    }
+}
